1999c16883bc590dca7803d53221bdbf11b6fb25
Yura <Yura@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2011-11-22 22:02:51 +0000

    Add database project

---
+++ EyeTracker/EyeTracker/Database/Schema Objects/Database Level Objects/Security/Schemas/Utils.schema.sql
@@ -0,0 +1,3 @@
+﻿CREATE SCHEMA [Utils]
+    AUTHORIZATION [dbo];
+
---
+++ EyeTracker/EyeTracker/Database/Schema Objects/Schemas/dbo/Tables/Screen.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Screen] (
+    [Id]            BIGINT       IDENTITY (1, 1) NOT NULL,
+    [Width]         INT          NOT NULL,
+    [Height]        INT          NOT NULL,
+    [ApplicationId] INT          NULL,
+    [FileExtension] NVARCHAR (5) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC) WITH (ALLOW_PAGE_LOCKS = ON, ALLOW_ROW_LOCKS = ON, PAD_INDEX = OFF, IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF)
+);
+
