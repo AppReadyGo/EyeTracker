e7ff45cd7d1bc1d47099014bf63bdf29c1ddd314
Philip <Philip@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2011-12-11 22:12:31 +0000

    some comments added

--- EyeTracker.Domain/Model/Application.cs
+++ EyeTracker.Domain/Model/Application.cs
@@ -6,20 +6,54 @@ using EyeTracker.Domain.Model.BackOffice;
 
 namespace EyeTracker.Domain.Model
 {
+
+    /// <summary>
+    /// this class represents application data mode 
+    /// instance of this class holds information about one single applications 
+    /// and describes application properties 
+    /// </summary>
     public class Application
     {
+        /// <summary>
+        /// application ID 
+        /// </summary>
         public virtual int Id { get; protected set; }
+        /// <summary>
+        /// Application description
+        /// </summary>
         public virtual string Description { get; protected set; }
+        /// <summary>
+        /// Application creation date - when the application created in our system 
+        /// </summary>
         public virtual DateTime CreateDate { get; protected set; }
+        /// <summary>
+        /// Member to represent OS platform type 
+        /// </summary>
         public virtual ApplicationType Type { get; protected set; }
+        /// <summary>
+        /// this member holds Portfolio 
+        /// </summary>
         public virtual Portfolio Portfolio { get; protected set; }
+        /// <summary>
+        /// holds application screens list 
+        /// </summary>
         public virtual IEnumerable<Screen> Screens { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public Application()
         {
             Screens = new List<Screen>();
         }
 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="portfolio"></param>
+        /// <param name="description"></param>
+        /// <param name="type"></param>
         public Application(Portfolio portfolio, string description, ApplicationType type)
         {
             this.CreateDate = DateTime.UtcNow;
--- EyeTracker.Domain/Model/ApplicationType.cs
+++ EyeTracker.Domain/Model/ApplicationType.cs
@@ -5,12 +5,30 @@ using System.Text;
 
 namespace EyeTracker.Domain.Model
 {
+    /// <summary>
+    /// Enum represents Application platform types
+    /// </summary>
     public enum ApplicationType
     {
+        /// <summary>
+        /// web application
+        /// </summary>
         Web = 1,
+        /// <summary>
+        /// webmobile application
+        /// </summary>
         WebMobile,
+        /// <summary>
+        /// android os based application
+        /// </summary>
         Android,
+        /// <summary>
+        /// iOS based applications
+        /// </summary>
         iPhone,
+        /// <summary>
+        /// windows mobile based application
+        /// </summary>
         WindowsMobile
     }
 }
