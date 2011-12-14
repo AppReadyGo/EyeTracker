493b6350bf57f07fd6fd9613d209c70459fa1628
Yura <Yura@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2011-12-14 22:59:06 +0000

    Add usage graph to portfolio dashboard

--- EyeTracker/EyeTracker/EyeTracker/Scripts/PortfolioDashboard.js
+++ EyeTracker/EyeTracker/EyeTracker/Scripts/PortfolioDashboard.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     var placeholder = $("#usage_charts_place_holder");
     var plot = $.plot(placeholder, usageChartData, {
         xaxis: { mode: "time", timeformat: '%d %b %y' },
-        yaxis: { autoscaleMargin: 0.02/*, tickFormatter: currencyFormatter */},
+        yaxis: { min: 0 },
         series: {
             lines: { show: true },
             points: { show: true }
