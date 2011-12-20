61f8ec3305f223c15138e84591655883dd3db16d
Yura <ypanshin@users.noreply.github.com>
2011-12-20 23:04:03 +0000

    Add date selector

--- EyeTracker/Scripts/portfolio.analytics.master.js
+++ EyeTracker/Scripts/portfolio.analytics.master.js
@@ -1,38 +1,52 @@
 ﻿
-
-$(function () {
-    var dates = $("#date_from, #date_to").datepicker({
-        defaultDate: portfolio.dateFrom,
-        dateFormat: 'dd M yy',
-        onSelect: function (selectedDate) {
-            var option = this.id == "date_from" ? "minDate" : "maxDate",
+function onSelect(selectedDate) {
+    var option = this.id == "date_from" ? "minDate" : "maxDate",
 					instance = $(this).data("datepicker"),
 					date = $.datepicker.parseDate(
 						instance.settings.dateFormat ||
 						$.datepicker._defaults.dateFormat,
 						selectedDate, instance.settings);
-            dates.not(this).datepicker("option", option, date);
-            if (this.id == "date_from") {
-                $('#date_from_lnk').text(selectedDate);
-                $('#date_from').val(selectedDate);
-            } else {
-                $('#date_to_lnk').text(selectedDate);
-                $('#date_to').val(selectedDate);
-            }
-        },
-        beforeShow: function (input, inst) {
-            var pos = $('#date_from_lnk').offset();
-            $.datepicker._pos = $.datepicker._findPos(input);
-            $.datepicker._pos[0] = pos.left;
-            $.datepicker._pos[1] = pos.top + $('#date_from_lnk').height() + 10;
-        }
+    $('#date_from_lnk,#datepicker_from').not(this).datepicker("option", option, date);
+    if (this.id == "date_from") {
+        $('#date_from_lnk').text(selectedDate);
+        $('#date_from').val(selectedDate);
+    } else {
+        $('#date_to_lnk').text(selectedDate);
+        $('#date_to').val(selectedDate);
+    }
+}
+
+function beforeShow(input, inst) {
+    var pos = $('#date_from_lnk').offset();
+    $.datepicker._pos = $.datepicker._findPos(input);
+    $.datepicker._pos[0] = pos.left;
+    $.datepicker._pos[1] = pos.top + $('#date_from_lnk').height() + 10;
+}
+
+$(function () {
+    $('#date_btn').click(function () {
+        $('#date_panel').show();
     });
-    $("#date_from").datepicker("option", "maxDate", portfolio.dateFromMax);
-    $("#date_to").datepicker("option", "minDate", portfolio.dateFromMin);
-    $('#date_from_lnk').click(function () {
-        $('#date_from').datepicker('show');
+    $("#datepicker_from").datepicker({
+        defaultDate: portfolio.dateFrom,
+        dateFormat: 'dd M yy',
+        onSelect: onSelect,
+        beforeShow: beforeShow,
+        maxDate: portfolio.dateFromMax,
+        minDate: portfolio.dateFromMin
     });
-    $('#date_to_lnk').click(function () {
-        $('#date_to').datepicker('show');
+    $("#datepicker_to").datepicker({
+        defaultDate: portfolio.dateTo,
+        dateFormat: 'dd M yy',
+        onSelect: onSelect,
+        beforeShow: beforeShow,
+        maxDate: portfolio.dateToMax,
+        minDate: portfolio.dateToMin
+    });
+    $('#apply_btn').click(function () {
+        var from = $("#datepicker_from").datepicker("getDate");
+        var to = $("#datepicker_to").datepicker("getDate");
+        $('#date_btn').text($.datepicker.formatDate('dd M yy', from) + ' - ' + $.datepicker.formatDate('dd M yy', to));
+        $('#date_panel').hide();
     });
 });
