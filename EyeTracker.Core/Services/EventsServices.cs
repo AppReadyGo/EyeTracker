9b2ae3c790fc2c2fc5946725c18ffef8edcdbc89
Philip <Philip@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2012-01-10 20:54:50 +0000

    some changes

--- EyeTracker.Core/Services/EventsServices.cs
+++ EyeTracker.Core/Services/EventsServices.cs
@@ -8,13 +8,141 @@ using EyeTracker.Domain.Repository;
 
 namespace EyeTracker.Core.Services
 {
+
+    /// <summary>
+    /// expose event services 
+    /// 4. HandlePackageEvent
+    /// </summary>
     public interface IEventsServices
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="viewPartEvent"></param>
+        /// <returns></returns>
+        OperationResult AddViewPartEvents(IEnumerable<ViewPartEvent> viewPartEvent);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="clickEvents"></param>
+        /// <returns></returns>
+        OperationResult AddClickEvents(IEnumerable<ClickEvent> clickEvents);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="visitEvents"></param>
+        /// <returns></returns>
+        OperationResult<long> AddVisitEvent(VisitEvent visitEvents);
+    
+        /// <summary>
+        /// Handle package event will procced operations with PackageEvent
+        /// </summary>
+        /// <param name="packageEvent"></param>
+        /// <returns>OK - if succeeded</returns>
+        OperationResult HandlePackageEvent(PackageEvent packageEvent);
+    }
+
+    public class EventsServices : IEventsServices
+    {
+        private IEventsRepository eventRepository = null;
+        private IDateRepository dataRepository = null;
+
+        public EventsServices()
+            : this(new EventsRepository(), new DataRepository())
+        {
+        }
+
+        public EventsServices(IEventsRepository eventRepository, IDateRepository dataRepository)
+        {
+            this.eventRepository = eventRepository;
+            this.dataRepository = dataRepository;
+        }
+
+        #region Old Code 
+        public OperationResult<long> AddVisitEvent(VisitEvent visitEvent)
+        {
+            try
+            {
+                //TODO: Add events writing by thread and queue
+                long eventId = eventRepository.AddVisitEvent(visitEvent);
+                //TODO: get country and city by ip
+                string country = string.Empty;
+                string city = string.Empty;
+                visitEvent.Id = eventId;
+                dataRepository.ParseVisitEvent(visitEvent, country, city);
+                return new OperationResult<long>(eventId);
+            }
+            catch (Exception exp)
+            {
+                return new OperationResult<long>(exp);
+            }
+        }
+
+        public OperationResult AddViewPartEvents(IEnumerable<ViewPartEvent> viewPartEvents)
+        {
+            try
+            {
+                eventRepository.AddViewPartEvents(viewPartEvents);
+                dataRepository.ParseViewPartEvents(viewPartEvents);
+                return new OperationResult();
+            }
+            catch (Exception exp)
+            {
+                return new OperationResult(exp);
+            }
+        }
+
+        public OperationResult AddClickEvents(IEnumerable<ClickEvent> clickEvents)
+        {
+            try
+            {
+                eventRepository.AddClickEvents(clickEvents);
+                dataRepository.ParseClickEvents(clickEvents);
+                return new OperationResult();
+            }
+            catch (Exception exp)
+            {
+                return new OperationResult(exp);
+            }
+        }
+        #endregion Old Code
+
+        public OperationResult HandlePackageEvent(PackageEvent packageEvent)
+        {
+            try
+            {
+
+            }
+            catch (Exception exp)
+            {
+                exp.ToString();
+            }
+            //always return OK
+            return new OperationResult();
+        }
+
+        #region IEventsServices Members
+
+       
+        #endregion
+    }
+
+    #region OldCode
+    /*
+     
+    public interface IEventsServices
+    {
+
         OperationResult AddViewPartEvents(IEnumerable<ViewPartEvent> viewPartEvent);
 
         OperationResult AddClickEvents(IEnumerable<ClickEvent> clickEvents);
 
         OperationResult<long> AddVisitEvent(VisitEvent visitEvents);
+    
+        OperationResult HandlePackageEvent(PackageEvent packageEvent);
+    
     }
 
     public class EventsServices : IEventsServices
@@ -80,4 +208,7 @@ namespace EyeTracker.Core.Services
             }
         }
     }
+     
+     */
+    #endregion
 }
---
+++ EyeTracker.Model/ObjectSerializator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace EyeTracker.Common
+{
+    public class ObjectSerializator<T>
+    {
+
+        private static Dictionary<Type, DataContractSerializer> m_serializers = 
+            new Dictionary<Type, DataContractSerializer>();
+
+
+        public static byte[] Serialize(object obj)
+        {
+            return null;
+        }
+
+        public static T Deserialize(byte[] arr)
+        {
+            return default(T);
+        }
+
+    }
+}
