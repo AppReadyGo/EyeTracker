244a2570797ecbfb693f01eacda90490d66efe29
Pavel <Pavel@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2012-05-22 04:15:11 +0000

    Submit Data added...

---
+++ EyeTracker/EyeTracker/EyeTracker.API/FPData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace EyeTracker.API
+{
+
+    [DataContract()]
+    public class FPData
+    {
+        [DataMember()]
+        public string InstanceValue { get; set; }
+    }
+}
