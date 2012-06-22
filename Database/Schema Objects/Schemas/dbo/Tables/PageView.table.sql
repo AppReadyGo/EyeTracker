aeb5904ff6e4e2160d81b854fc0b6b1e6ab38ed3
Yura <ypanshin@users.noreply.github.com>
2012-06-22 19:14:24 +0000

    Fix database

--- Database/Schema Objects/Schemas/dbo/Tables/PageView.table.sql
+++ Database/Schema Objects/Schemas/dbo/Tables/PageView.table.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[PageView] (
-    [Id]                BIGINT         NOT NULL,
+    [Id]                BIGINT IDENTITY (1, 1) NOT NULL,
     [Date]              DATETIME       NOT NULL,
     [Path]              NVARCHAR (256) NOT NULL,
     [Ip]                NVARCHAR (15)  NULL,
@@ -15,4 +15,25 @@
     [ApplicationId]     INT            NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC) WITH (ALLOW_PAGE_LOCKS = ON, ALLOW_ROW_LOCKS = ON, PAD_INDEX = OFF, IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF)
 );
+GO
+
+ALTER TABLE [dbo].[PageView]
+ADD CONSTRAINT [FK_PageView_Browser] FOREIGN KEY ([BrowserId]) REFERENCES [dbo].[Browser] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+ALTER TABLE [dbo].[PageView]
+ADD CONSTRAINT [FK_PageView_Language] FOREIGN KEY ([LanguageId]) REFERENCES [dbo].[Language] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+ALTER TABLE [dbo].[PageView]
+ADD CONSTRAINT [FK_PageView_OperationSystem] FOREIGN KEY ([OperationSystemId]) REFERENCES [dbo].[OperationSystem] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+ALTER TABLE [dbo].[PageView]
+ADD CONSTRAINT [FK_PageView_Application] FOREIGN KEY ([ApplicationId]) REFERENCES [dbo].[Application] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+ALTER TABLE [dbo].[PageView]
+ADD CONSTRAINT [FK_PageView_Countries] FOREIGN KEY ([CountryId]) REFERENCES [dbo].[Countries] ([GeoId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
 
--- Database/Schema Objects/Schemas/dbo/Tables/Scroll.table.sql
+++ Database/Schema Objects/Schemas/dbo/Tables/Scroll.table.sql
@@ -5,4 +5,9 @@
     [LastTouchId]        BIGINT NOT NULL,
     [PageViewId]         BIGINT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC) WITH (ALLOW_PAGE_LOCKS = ON, ALLOW_ROW_LOCKS = ON, PAD_INDEX = OFF, IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF)
-)
+);
+GO
+
+ALTER TABLE [dbo].[Scroll]
+ADD CONSTRAINT [FK_Scroll_PageView] FOREIGN KEY ([PageViewId]) REFERENCES [dbo].[PageView] ([Id]);
+GO
