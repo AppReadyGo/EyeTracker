0836742d589d02f8f59875478a7e932c10019f01
Yura <ypanshin@users.noreply.github.com>
2012-06-27 18:28:58 +0000

    Remove no used classes

---
+++ EyeTracker.Domain/Overwrite/Domain/Enumerators.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EyeTracker.DAL.Domain
+{
+
+    public enum UserActivityType
+    {
+        LogIn,
+        LogOut,
+        AddApplication,
+        EditApplication,
+        RemoveApplication
+    }
+}
---
+++ EyeTracker.Domain/Overwrite/Domain/UserActivity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EyeTracker.DAL.Domain
+{
+    public class UserActivity
+    {
+        public DateTime Date { get; set; }
+
+        public int UserId { get; set; }
+
+        public UserActivityType ActivityType { get; set; }
+
+        public string Description { get; set; }
+
+        public int? LinkedObjectId { get; set; }
+    }
+}
