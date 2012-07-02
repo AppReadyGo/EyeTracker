aac7757c0ab08c728aa2b05861299ec110a6568f
Yura <ypanshin@users.noreply.github.com>
2012-07-02 20:14:16 +0000

    Fix

--- Database/Scripts/DevelopmentData.sql
+++ Database/Scripts/DevelopmentData.sql
@@ -2,8 +2,8 @@
 -- Script initilizate database with development data
 -- =============================================
 
-INSERT INTO [usr].[Users] ([UserTypeID], [Email], [Password], [PasswordSalt], [CreateDate], [Activated], [FirstName], [LastName],[Unsubscribed])
-VALUES (1/*Staff*/, 'dev@mobillify.com', 'XW0mA5DzfN4XL851H/i1xNFFbMOdtjVAL6fjBN5monE='/*111111*/, '/WCjbQ==', '20120101', 1, 'Development', 'Mobillify', 0);
+INSERT INTO [usr].[Users] ([UserTypeID], [Email], [Password], [PasswordSalt], [CreateDate], [Activated], [FirstName], [LastName],[Unsubscribed], [SpecialAccess])
+VALUES (1/*Staff*/, 'dev@mobillify.com', 'XW0mA5DzfN4XL851H/i1xNFFbMOdtjVAL6fjBN5monE='/*111111*/, '/WCjbQ==', '20120101', 1, 'Development', 'Mobillify', 0, 0);
 
 GO
 
