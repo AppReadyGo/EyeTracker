03005b9dfaee328ef07ff897512af00ddb4ebe03
Pavel <pavel.mogilevsky@gmail.com>
2012-07-30 14:55:38 +0000

    crear DB

--- Database/Scripts/ClearUserData.sql
+++ Database/Scripts/ClearUserData.sql
@@ -1,8 +1,8 @@
 ﻿-- =============================================
 -- Clears all user data
 -- =============================================
-  delete from [Fingerprint].[dbo].[PackageEvent];
-  delete from [Fingerprint].[dbo].[SessionInfoEvent];
-  delete from [Fingerprint].[dbo].[ScrollEvent];
-  delete from [Fingerprint].[dbo].[ClickEvent];
-  delete from [Fingerprint].[dbo].[ViewPartEvent];
+  delete from [dbo].[PackageEvent];
+  delete from [dbo].[SessionInfoEvent];
+  delete from [dbo].[ScrollEvent];
+  delete from [dbo].[ClickEvent];
+  delete from [dbo].[ViewPartEvent];
