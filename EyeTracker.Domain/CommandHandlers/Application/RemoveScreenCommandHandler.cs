dad9fcc95974a0eab1ec0e15085e7d7b19546975
Yura <ypanshin@users.noreply.github.com>
2012-08-03 22:05:37 +0000

    SQL Scripts Redesign screens portfolios applications

---
+++ EyeTracker.Domain/CommandHandlers/Application/RemoveScreenCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using EyeTracker.Common.Commands;
+using EyeTracker.Domain.Model;
+using NHibernate;
+using EyeTracker.Common.Commands.Application;
+
+namespace EyeTracker.Domain.CommandHandlers.Application
+{
+    public class RemoveScreenCommandHandler : ICommandHandler<RemoveScreenCommand, int>
+    {
+        public int Execute(ISession session, RemoveScreenCommand cmd)
+        {
+            var screen = session.Get<Model.Screen>(cmd.Id);
+            screen.Application.RemoveScreen(screen);
+            session.Delete(screen);
+            return screen.Id;
+        }
+    }
+}
