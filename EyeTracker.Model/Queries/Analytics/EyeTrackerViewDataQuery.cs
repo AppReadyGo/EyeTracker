b72076d99b5c5f1bb37c0bc588f696a953a002c4
Yura <ypanshin@users.noreply.github.com>
2012-12-16 19:52:20 +0000

    Add query

---
+++ EyeTracker.Model/Queries/Analytics/EyeTrackerViewDataQuery.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EyeTracker.Common.QueryResults.Analytics.QueryResults;
+using System.Drawing;
+
+namespace EyeTracker.Common.Queries.Analytics
+{
+    public class EyeTrackerViewDataQuery : IQuery<EyeTrackerViewDataResult>, IFilterQuery
+    {
+        public DateTime From { get; private set; }
+        public DateTime To { get; private set; }
+        public int? PortfolioId { get; private set; }
+        public int? ApplicationId { get; private set; }
+        public Size? ScreenSize { get; private set; }
+        public string Path { get; private set; }
+        public string Language { get; private set; }
+        public string OperatingSystem { get; private set; }
+        public string Country { get; private set; }
+        public string City { get; private set; }
+
+        public EyeTrackerViewDataQuery(
+            DateTime from,
+            DateTime to,
+            int? portfolioId,
+            int? applicationId,
+            Size? screenSize,
+            string path,
+            string language,
+            string operatingSystem,
+            string country,
+            string city)
+        {
+            this.From = from.StartDay();
+            this.To = to.EndDay();
+            this.PortfolioId = portfolioId;
+            this.ApplicationId = applicationId;
+            this.ScreenSize = screenSize;
+            this.Path = path;
+            this.Language = language;
+            this.OperatingSystem = operatingSystem;
+            this.Country = country;
+            this.City = city;
+        }
+    }
+}
---
+++ EyeTracker/Model/Pages/Analytics/EyeTrackerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EyeTracker.Model.Filter;
+using EyeTracker.Common.QueryResults.Analytics;
+
+namespace EyeTracker.Model.Pages.Analytics
+{
+    public class EyeTrackerModel : FilterModel
+    {
+        public int PointsOnReport { get; set; }
+
+        public IEnumerable<ScreenResult> Screens { get; set; }
+    }
+}
