11317e5e678a9c568faba4ec527329df7c07a3f4
Yura <Yura@f17c476e-fc41-43f8-84d0-f8b84ac3c184>
2012-12-28 19:49:37 +0000

    Add GaDoNet

---
+++ EyeTracker/EyeTracker/ThridParty/GaDotNet1.2/Source/GaDotNet.Common/Data/GoogleProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GaDotNet.Common.Data
+{
+	public class GoogleProduct
+	{
+		public string ProductName { get; set; }
+
+		public string ProductSku { get; set; }
+
+		public string ProductVariant { get; set; }
+
+		public decimal UnitPrice { get; set; }
+	}
+}
