15b4f0e210c364be3562effb2cc68d7082b64e16
Pavel <pavel.mogilevsky@gmail.com>
2012-12-28 21:35:59 +0000

    service added

---
+++ EyeTracker.API/Data/MarketData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace EyeTracker.API.Data
+{
+    [DataContract()]
+    public class MarketData
+    {
+
+        [DataMember(Name="uid")]
+        public string User { get; set; }
+
+        [DataMember(Name="data")]
+        public string Data { get; set; }
+    }
+}
